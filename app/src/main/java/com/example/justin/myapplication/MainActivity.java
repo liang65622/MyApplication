7ba030a2d29881b10146907a684dde42cfdd6c24
Justin <zik8000@126.com>
2017-10-08 21:14:25 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/justin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justin/myapplication/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int a = 1;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
